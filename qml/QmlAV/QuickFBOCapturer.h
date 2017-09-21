1640326dd65aef3ab1d8b15fddef6f4902d907a3
Francisco Moya <francisco.moya@displaynote.com>
2017-09-21 20:18:48 +0200

    Add AVCapturer in QuickFBOCapturer

--- qml/QmlAV/QuickFBOCapturer.h
+++ qml/QmlAV/QuickFBOCapturer.h
@@ -2,19 +2,27 @@
 #define QTAV_QUICKFBOCAPTURER_H
 
 #include <QmlAV/QuickFBORenderer.h>
+#include <QtAV/AVVideoCapturer.h>
 
 namespace QtAV {
 class QuickFBOCapturer: public QuickFBORenderer
 {
     Q_OBJECT
+    Q_PROPERTY(AVVideoCapturer* capturer READ capturer WRITE setCapturer NOTIFY capturerChanged)
 public:
     explicit QuickFBOCapturer(QQuickItem *parent = 0);
 
-Q_SIGNALS:
-    void frameCaptured(const QtAV::VideoFrame & frame);
+    AVVideoCapturer *capturer() const;
+    void setCapturer(AVVideoCapturer *capturer);
 
 protected:
     virtual bool receiveFrame(const VideoFrame &frame) Q_DECL_OVERRIDE;
+
+Q_SIGNALS:
+    void capturerChanged();
+
+private:
+    AVVideoCapturer *m_capturer;
 };
 } // namespace QtAV
 #endif // QTAV_QUICKFBOCAPTURER_H
--- qml/QuickFBOCapturer.cpp
+++ qml/QuickFBOCapturer.cpp
@@ -1,12 +1,31 @@
 #include "QmlAV/QuickFBOCapturer.h"
 
 namespace QtAV {
-QuickFBOCapturer::QuickFBOCapturer(QQuickItem *parent) : QuickFBORenderer(parent) {
+QuickFBOCapturer::QuickFBOCapturer(QQuickItem *parent) :
+    QuickFBORenderer(parent),
+    m_capturer(nullptr)
+{
 }
 
 bool QuickFBOCapturer::receiveFrame(const VideoFrame &frame)
 {
-    emit frameCaptured(frame);
+    if (m_capturer) {
+        m_capturer->setFrame(frame, orientation());
+    }
+
     return QuickFBORenderer::receiveFrame(frame);
+}
+
+AVVideoCapturer *QuickFBOCapturer::capturer() const
+{
+    return m_capturer;
+}
+
+void QuickFBOCapturer::setCapturer(AVVideoCapturer *capturer)
+{
+    if (m_capturer != capturer) {
+        m_capturer = capturer;
+        Q_EMIT capturerChanged();
+    }
 }
 } // namespace QtAV
